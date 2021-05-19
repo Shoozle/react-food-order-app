49381d4fa6fb2734ab8eb20f345c77861ee49e00
Shoozle <sean@seanmurtagh.uk>
2021-05-19 16:49:01 +0100

    Remove items functionality

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -29,9 +29,31 @@ const cartReducer = (state, action) => {
             updatedItems = state.items.concat(action.item);
         }
         
-        
         return {
             items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
+    if (action.type === 'REMOVE_ITEM') {
+        
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id);
+            
+        const existingItem = state.items[existingCartItemIndex]
+        const updatedTotalAmount = state.totalAmount - existingItem.price
+
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = {...existingItem, amount: existingItem.amount - 1}
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items:updatedItems,
             totalAmount: updatedTotalAmount
         }
     }
@@ -48,7 +70,7 @@ const CartProvider = props => {
     }
 
     const removeItemToCartHandler = id => {
-        dispatchCartAction({type: 'REMOVE_IEM', id: id})
+        dispatchCartAction({type: 'REMOVE_ITEM', id: id})
     }
 
 
