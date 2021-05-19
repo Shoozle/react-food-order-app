fc35ebfeece73e521f3c71c184c39952fae976eb
Shoozle <sean@seanmurtagh.uk>
2021-05-19 13:30:28 +0100

    Modals, overlays and portals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Cart from './components/Cart/Cart';
 import Header from "./components/Layout/Header";
 import MealDeals from './components/MealDeals/MealDeals';
 
 function App() {
   return (
     <>
+      <Cart/>
       <Header />
       <main>
         <MealDeals />
